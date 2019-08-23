053bc0a59485cc852425e5850d7d022f26eb60af
Adrian-Garcia <agarc5@hotmail.com>
2019-08-23 11:33:47 -0500

    added axios to location list for R&M locations

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
-export default function LocationsList() {}
+//https://rickandmortyapi.com/api/locations/
+
+export default function LocationsList() {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        Axios
+            .get(`https://rickandmortyapi.com/api/location/`)
+            .then(res =>{
+                setData(res.data.results)
+                console.log('respons',res)
+            })
+            .catch(err => {
+                console.log('error', err);
+              })
+    },[])
+
+    return <section>
+
+        {console.log('Location',data)}
+    </section>
+    
+}
