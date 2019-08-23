e65a73b29badeb84fa695d9c9f064f544ebb306b
Adrian-Garcia <agarc5@hotmail.com>
2019-08-23 10:42:16 -0500

    edits on character card & character list

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
-export default function CharacterCard({props}) {
-  return <span>todo: character</span>;
+export default function CharacterCard (props) {
+  return (
+    <Card>
+    <Image src={props.img} wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>{props.name}</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2019</span>
+      </Card.Meta>
+      <Card.Description>
+        Adrian is new to Austin, Texas!
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
+  )
 }
